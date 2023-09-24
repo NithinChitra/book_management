93a4be0c678828d290d80e3eabf65212630663e7
NithinChitra <nithink24770@gmail.com>
2023-09-24 13:09:26 +0530

    Book_Management

--- controller/userController.js
+++ controller/userController.js
@@ -22,30 +22,38 @@ exports.getLogin = async(req,res)=>{
         
     }
 }
+// const bcrypt = require('bcrypt');
 exports.verifyLogin = async(req,res)=>{
     try{
         const {email,password} = req.body;
-        const existingEmail = User.findOne({email});
-        if(existingEmail){
-         
+        const existingEmail = await User.findOne({email});
+        if(!existingEmail){
+            return res.status(401).json({error: 'Invalid email or password'});
         }
+        if(existingEmail.password == password){
+            console.log("ACCEPTED");
+        }
+        else{
+            console.log("Not Accepted");
+        }
+        // const passwordMatch = await bcrypt.compare(password,existingUser.password);
         return res.redirect('back');
     }
     catch(err){
-        
+        console.error('Error in verifying login:', err);
+        return res.redirect('back');
     }
 }
 exports.createUser = async(req,res)=>{
     try{
         const {name,password,email} = req.body;
-        const existingEmail = User.findOne({email})
+        const existingEmail = await User.findOne({email})
             if(existingEmail){
                 return res.send("Error email id is already present");
             }
-            return User.create({username,email,password})
-            const userCreated =  User.create({name,password,email});
-            console.log('User Registered Successfully',userCreated);
-            return res.redirect('back');
+        const userCreated = await User.create({name,password,email});
+        console.log('User Registered Successfully',userCreated);
+        return res.redirect('back');
         
     }
     catch(err){
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ const userController = require('../controller/userController');
 
 Router.get('/',userController.getPage);
 Router.get('/login',userController.getLogin);
-// Router.get('/verify-login',userController.verifyLogin);
+Router.post('/verify-login',userController.verifyLogin);
 Router.post('/create-user',userController.createUser);
 module.exports = Router
